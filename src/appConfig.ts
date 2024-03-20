4fc46eafda61d0ba26b7f8d7eca7ed4c48fe5219
John Hillert <john.hillert@gmail.com>
2024-03-20 13:13:54 -0500

    invalidate accidentally committed DSN and generate new one (untracked)

--- src/appConfig.ts
+++
@@ -1,4 +0,0 @@
-export const SENTRY_ENDPOINT = ''; // A valid DSN needs to be provided for this branch to work.
-export const APP_BUILD = '1';
-export const APP_VERSION = '0.0';
-export const SENTRY_ENABLED = 'true';
